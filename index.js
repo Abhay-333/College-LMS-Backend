e160a543432f6c60461746286e2e3dae6a955ee6
Abhay-333 <abhayd0331@gmail.com>
2025-08-01 16:08:30 +0530

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import express from "express";
+import dbConnection from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "https://college-lms-2.onrender.com",
+      "http://localhost:5173", 
+    ],
+    credentials: true,
+  })
+);
+
+app.use("/api", authRouter);
+
+app.listen(process.env.PORT || 5000, () => {
+  dbConnection();
+});
